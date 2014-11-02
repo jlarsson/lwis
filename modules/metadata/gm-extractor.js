66a0d3e8ee2149d715eb2f3c16a2f7018a028ce4
jlarsson <mimmertim@gmail.com>
2014-11-02 17:45:33 +0100

    lwip -> gm

---
+++ modules/metadata/gm-extractor.js
@@ -0,0 +1,32 @@
+(function (module){
+    'use strict';
+    
+    var gm = require('gm');
+    var debug = require('debug')('lwis:gm-extractor');
+
+    function extract(app, options, cb){
+        var metadata = options.metadata;
+        
+        if ((options.metadata.mimetype||'').indexOf('image/') !== 0){
+            debug('skipping mimetype %s', options.metadata.mimetype);
+            return cb();
+        }
+        
+        
+        gm(options.path).size(function (err, size){
+            if (err){
+                debug('error: %j', err);
+                return cb(err);
+            }
+            debug('image size: %j', size);
+            var imd = metadata.image || (metadata.image = {});
+            imd.width = size.width;
+            imd.height = size.height;
+            cb();
+        });
+    };
+    
+    
+    module.exports = extract;
+
+})(module);
