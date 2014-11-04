6f5ae84dfbb963a704e0537adc4b6a8383d2c10c
jlarsson <mimmertim@gmail.com>
2014-11-04 09:24:32 +0100

    encoding detection for text/*

--- modules/metadata/chardet.js
+++ modules/metadata/chardet.js
@@ -1,27 +1,24 @@
 (function (module){
     'use strict';
     
-    var gm = require('gm');
+    var chardet = require('chardet');
     var debug = require('debug')('lwis:gm-extractor');
 
     function extract(app, options, cb){
         var metadata = options.metadata;
         
-        if ((metadata.mimetype||'').indexOf('image/') !== 0){
-            debug('skipping mimetype %s', metadata.mimetype);
+        if ((metadata.mimetype||'').indexOf('text/') !== 0){
             return cb();
         }
         
-        
-        gm(options.path).size(function (err, size){
+        chardet.detectFile(options.path, function (err, encoding){
             if (err){
                 debug('error: %j', err);
                 return cb(err);
             }
-            debug('image size: %j', size);
-            var imd = metadata.image || (metadata.image = {});
-            imd.width = size.width;
-            imd.height = size.height;
+            if (encoding){
+                metadata.charset = encoding;
+            }
             cb();
         });
     };
